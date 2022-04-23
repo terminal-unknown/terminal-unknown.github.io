8e277b452f86b75f683db334c40c21e7fec39561
Elena Miller <emiller242@my.stlcc.edu>
2022-04-23 13:11:48 -0500

    css styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('./package.json'),
         build:{
             
         }
--- root/Assets/js/terminal.js
+++ root/Assets/js/terminal.js
@@ -4,9 +4,12 @@
  * 
  */
 
-const togglePower = function(){
-    
-    };
+function checkElement(e) {
+
+}
+const togglePower = function () {
+
+};
 $(document).ready(() => {
     $("#selterm").change(evt => {
         let tnum = $("#selterm").val();
@@ -17,10 +20,7 @@ $(document).ready(() => {
 //    document.getElementById("selterm").onchange = () => {
 //       
 //    };
-    document.getElementById("selterm").onchange = (evt) => {
-        var s = document.getElementById("selterm").value;
-        $("term").val(s);
-        $("term").text(s);
+    document.getElementById("connect").onchange(evt => {
         let d = parseInt($("connect").val());
         switch (d) {
             case - 1:
@@ -38,48 +38,55 @@ $(document).ready(() => {
                 $("#connect").text('C0#n3c7eD_');
                 break;
             default:
-                $("#connect");
+                $("#connect").text('Connection Error. Try Again Later.');
                 break;
 
-                
+        }
+    });
+    document.getElementById("selterm").onchange = (evt) => {
+
+        var s = document.getElementById("selterm").value;
+        var t = document.getElementById("power").innerHTML;
+        var cstate = parseInt(document.getElementById("connect").value);
+        if (cstate === 1) {
+            $("#msg").text("Cannot Disconnect")
+        } else {
+            $("term").val(s);
+            $("term").text(s);
+
+        }
+
+        evt.preventDefault();
+    };
+
+    $("#power").click(evt => {
+        var e = document.getElementById("power");
+
+        var p = document.getElementById("crt");
+        if (p.classList.contains("off")) {
+            p.classList.replace("off", "on");
+            $("#connect").val("0");
+            var w = 0;
+            while (w > 10) {
+                w++;
+            }
+            $("#connect").val("1");
+            $("#power").text("Disconnect")
+        } else if (p.classList.contains("on")) {
+            p.classList.replace("on", "off");
+            $("#connect").val("-1");
         }
         evt.preventDefault();
-    } ;
-//        document.getElementById("power").onclick = (evt)=>{
-//            
-//            var p = document.getElementById("crt");
-//            if (p.classList.contains("off")) {
-//                p.classList.replace("off", "on");
-//                $("#connect").val("0");
-//                var w = 0;
-//                while (w > 10){
-//                    w++;
-//                }
-//                $("#connect").val("1");
-//
-//            } else if (p.classList.contains("on")) {
-//                p.classList.replace("on", "off");
-//                $("#connect").val("-1");
-//            }
-//            evt.preventDefault();
-//        };
-        $("#power").click(evt => {
-
-            var p = document.getElementById("crt");
-            if (p.classList.contains("off")) {
-                p.classList.replace("off", "on");
-                $("#connect").val("0");
-                var w = 0;
-                while (w > 10){
-                    w++;
+    });
+    $("details summary").click(evt => {
+        let e = document.getElementsByTagName("summary");
+        let target = evt.currentTarget;
+        for (var i = 0; i < e.length; i++) {
+            if (e[i] !== target) {
+                if (e[i].parentElement.hasAttribute("open")) {
+                    e[i].parentElement.removeAttribute("open", null);
                 }
-                $("#connect").val("1");
-                $("#power").text("Disconnect")
-            } else if (p.classList.contains("on")) {
-                p.classList.replace("on", "off");
-                $("#connect").val("-1");
             }
-            evt.preventDefault();
-        });
-   
+        }
+    });
 });
